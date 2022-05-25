87f9953822263849e5026be0ab2d3e1e73d66f60
Griffin <griffinmmatthews@gmail.com>
2022-05-24 23:23:17 -0400

    fixed carousel

--- src/components/MovieBanner.js
+++ src/components/MovieBanner.js
@@ -2,16 +2,17 @@ import React, {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
 import requests from '../shared/requests'
 import instance from '../axios'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel'
 
 function MovieBanner() {
-    const [movies, setMovie] =useState([])
+    const [movies, setMovies] =useState([])
     const base_url = "https://image.tmdb.org/t/p/original"
     useEffect(() =>{
         async function fetchData(){
             const response = await instance.get(requests.fetchPopular);
             const filteredResponse = response.data.results.filter(movie => movie.title && movie.backdrop_path)
-            setMovie(filteredResponse.slice(0, 5))
+            setMovies(filteredResponse.slice(0, 5))
             return response
         }
         fetchData()
@@ -19,24 +20,25 @@ function MovieBanner() {
 
   return (
     <Carousel
-        showThumbs={false}
+        infiniteLoop={true}
         showStatus={false}
         autoPlay
         interval={10000}
-        infiniteLoop={true}
         showArrows={false}
         transitionTime={500}
+        showThumbs={false}
+        key={movies}
     >
-        {movies.map(movie=>{
+        {movies.map((movie, index)=>{
             return(
-                <div className="movieBanner" key={movie.id}>
+                <div className="movieBanner" key={index}>
                     <div>
                         <Link to={`/${movie.id}`}>
                             <h1>{movie.title}</h1>
                         </Link>
                         <img src={`${base_url}${movie.backdrop_path}`} alt={movie.title}/>
                     </div>
-                    <span className="bannerFade"/>
+                    
                 </div>
             )
         })}
