f6e6a59f44c10483bdc028a0f8df25625b827d64
Griffin <griffinmmatthews@gmail.com>
2022-08-24 15:56:50 -0400

    added debounce to search to limit amount of api requests

--- src/components/screens/MovieSearchScreen.js
+++ src/components/screens/MovieSearchScreen.js
@@ -5,10 +5,23 @@ import requests from '../../shared/requests'
 function MovieSearch() {
 
   const [searchMovie, setSearchMovie]= useState('a')
+  let timer
+
+  const updateMovieSearch = (event) => {
+    return searchTerm(event.target.value)
+  }
+
+  const searchTerm = (value) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      setSearchMovie(value?value:"a")
+      console.log(value)
+    }, 700);
+  }
 
   return (
     <>
-      <input type="search" placeholder="Search For a Movie..." className="searchBar" onChange={event=> setSearchMovie(event.target.value)}/>
+      <input type="search" placeholder="Search For a Movie..." className="searchBar" onChange={event=> updateMovieSearch(event)}/>
       <MovieRow request={`${requests.fetchSearch}${searchMovie}`} search={true}/>
     </>
   )
